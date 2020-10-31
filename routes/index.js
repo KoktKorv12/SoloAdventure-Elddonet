9ca71c47d485be2daf6f8d43e34a06b23e314154
KoktKorv12 <redguyshrug@gmail.com>
2020-10-31 15:37:23 +0100

    Added homepage

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,8 @@ var router = express.Router();
 const { pool } = require('../models/db');
 
 /* GET home page. */
-router.get('/', function (request, resolve, next) {
-  const sql = 'SELECT * FROM links';
-
-  pool.query(sql, request.params.id, function (err, result, fields) {
-    if (err) throw err;
-    resolve.json({
-      status: 200,
-      result
-    });
-  });
+router.get('/', function (req, res, next) {
+  res.render('frontpage');
 });
 
 module.exports = router;
